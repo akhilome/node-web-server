e90a27ed7e61939ac86aba44f429adc3f312202a
Kizito Akhilome <kizito@akhilo.me>
2018-09-15 13:45:31 +0100

    Start templating with handlebars

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs')
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to express'
+  res.render('home', {
+    title: 'Hompage',
+    year: new Date().getFullYear(),
+    message: 'Welcome home!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    year: new Date().getFullYear()
   });
 });
 
